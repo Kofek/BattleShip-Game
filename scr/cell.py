f5c443b0ae9d6d088c1f7826720236f041ffea91
Kacper <Opasu3@gmail.com>
2025-07-09 14:09:41 +0200

    New version, a new inscription has been added, describing the game state

--- scr/cell.py
+++ scr/cell.py
@@ -18,6 +18,7 @@ class Cell:
     player2_MarkedCells = []
     player1_ships_count_label_object = None
     player2_ships_count_label_object = None
+    message_label_object = None
 
     def __init__(self, x, y):
         self.x=x
@@ -39,6 +40,42 @@ class Cell:
         btn.bind('<Button-1>', self.left_click_actions) # left click
         self.cell_btn_object = btn
 
+    def create_player1_ship_count(location):
+        lbl = Label(
+            location,
+            bg="black",
+            fg="white",
+            width=20,
+            height=2,
+            text=("Player1 ships left: 5"),
+            font=("", 21)
+        )
+        Cell.player1_ships_count_label_object = lbl
+
+    def create_player2_ship_count(location):
+        lbl = Label(
+            location,
+            bg="black",
+            fg="white",
+            width=20,
+            height=2,
+            text=("Player2 ships left: 5"),
+            font=("", 21)
+        )
+        Cell.player2_ships_count_label_object = lbl
+
+    def create_message_label(location):
+        lbl = Label(
+            location,
+            bg="black",
+            fg="white",
+            width=50,
+            height=2,
+            text=("Player1 chooses ships."),
+            font=("", 21)
+        )
+        Cell.message_label_object = lbl
+
     @staticmethod
     def player_block(cell_List):
         for cell in cell_List:
@@ -49,6 +86,67 @@ class Cell:
         for cell in cell_list:
             if cell not in marked_list and cell not in guessed_list:
                 cell.cell_btn_object.bind('<Button-1>', cell.left_click_actions)
+
+    @staticmethod
+    def bulid_player_grid(name,location):
+        if name == "player1":
+            cell_list = Cell.player1_cells
+        elif name == "player2":
+            cell_list =Cell.player2_cells
+
+        for x in range(settings.button_rows):
+            for y in range(settings.button_columns):
+                button = Cell(x, y)
+                button.create_btn_object(
+                    location,
+                    height=settings.button_height,
+                    width=settings.button_width
+                )
+                button.cell_btn_object.grid(column=x, row=y)
+                cell_list.append(button)
+
+    @staticmethod
+    def end_game(winner):
+        ctypes.windll.user32.MessageBoxW(0, f"Congratulations, {winner} wins!", f"{winner} wins.", 0)
+        sys.exit()
+
+    @staticmethod
+    def update_ship_labels():
+        p1_remaining = len(Cell.player1_Ships) - len(Cell.player1_GuessedShips)
+        p2_remaining = len(Cell.player2_Ships) - len(Cell.player2_GuessedShips)
+
+        Cell.player1_ships_count_label_object.configure(text=f"Player1 ships left: {p1_remaining}")
+        Cell.player2_ships_count_label_object.configure(text=f"Player2 ships left: {p2_remaining}")
+
+    @staticmethod
+    def update_message_label():
+        if Cell.setup_phase:
+            if not Cell.player1_time:
+                Cell.message_label_object.configure(text=f"Player2 chooses ships.")
+            else:
+                Cell.message_label_object.configure(text=f"Player1 turn.")
+        else:
+            if Cell.player1_time:
+                Cell.message_label_object.configure(text=f"Player1 turn.")
+            else:
+                Cell.message_label_object.configure(text=f"Player2 turn.")
+
+    @staticmethod
+    def creating_labels(player1_location,player2_location,message_location):
+        Cell.create_player1_ship_count(player1_location)
+        Cell.player1_ships_count_label_object.place(x=0, y=0)
+        Cell.create_player2_ship_count(player2_location)
+        Cell.player2_ships_count_label_object.place(x=0, y=50)
+        Cell.create_message_label(message_location)
+        Cell.message_label_object.place(x=0, y=0)
+
+    def left_click_actions(self, event):
+        if not self.is_click_valid():
+            return
+        if Cell.setup_phase:
+            self.handle_setup_phase()
+        else:
+            self.handle_game_phase()
 
     def is_click_valid(self):
         return (
@@ -69,8 +167,7 @@ class Cell:
         ship_list.append(self)
 
         if len(ship_list) == 5:
-            Cell.player_block(Cell.player1_cells if Cell.player1_time else Cell.player2_cells)\
-
+            Cell.player_block(Cell.player1_cells if Cell.player1_time else Cell.player2_cells)
             if Cell.player1_time:
                 Cell.player1_time = False
                 Cell.switch_to_player2()
@@ -81,6 +178,7 @@ class Cell:
                 Cell.player_block(Cell.player2_cells)
                 Cell.player_reset(Cell.player1_cells, Cell.player1_MarkedCells, Cell.player1_GuessedShips)
                 Cell.player_reset(Cell.player2_cells, Cell.player2_MarkedCells, Cell.player2_GuessedShips)
+            Cell.update_message_label()
 
     def handle_game_phase(self):
         if Cell.player1_time:
@@ -92,6 +190,7 @@ class Cell:
                 Cell.player_block(Cell.player2_cells)
                 Cell.player1_time = False
                 Cell.player_reset(Cell.player1_cells, Cell.player1_MarkedCells, Cell.player1_GuessedShips)
+                Cell.update_message_label()
         else:
             if self in Cell.player1_cells:
                 if self.is_ship:
@@ -101,27 +200,7 @@ class Cell:
                 Cell.player_block(Cell.player1_cells)
                 Cell.player1_time = True
                 Cell.player_reset(Cell.player2_cells, Cell.player2_MarkedCells, Cell.player2_GuessedShips)
-
-    def left_click_actions(self, event):
-        if not self.is_click_valid():
-            return
-        if Cell.setup_phase:
-            self.handle_setup_phase()
-        else:
-            self.handle_game_phase()
-
-    @staticmethod
-    def end_game(winner):
-        ctypes.windll.user32.MessageBoxW(0, f"Congratulations, {winner} wins!", f"{winner} wins.", 0)
-        sys.exit()
-
-    @staticmethod
-    def update_ship_labels():
-        p1_remaining = len(Cell.player1_Ships) - len(Cell.player1_GuessedShips)
-        p2_remaining = len(Cell.player2_Ships) - len(Cell.player2_GuessedShips)
-
-        Cell.player1_ships_count_label_object.configure(text=f"Player1 ships left: {p1_remaining}")
-        Cell.player2_ships_count_label_object.configure(text=f"Player2 ships left: {p2_remaining}")
+                Cell.update_message_label()
 
     def register_hit(self, guessed_ships):
         guessed_ships.append(self)
@@ -147,29 +226,7 @@ class Cell:
         marked_cells = Cell.player1_MarkedCells if Cell.player1_time else Cell.player2_MarkedCells
         marked_cells.append(self)
 
-    def create_player1_ship_count(location):
-        lbl = Label(
-            location,
-            bg="black",
-            fg="white",
-            width=10,
-            height=2,
-            text=("Player1 ships left: 5"),
-            font=("", 21)
-        )
-        Cell.player1_ships_count_label_object = lbl
 
-    def create_player2_ship_count(location):
-        lbl = Label(
-            location,
-            bg="black",
-            fg="white",
-            width=10,
-            height=2,
-            text=("Player2 ships left: 5"),
-            font=("", 21)
-        )
-        Cell.player2_ships_count_label_object = lbl
 
     def get_cell_by_axis(self, x, y):
         for cell in Cell.all:
@@ -193,11 +250,3 @@ class Cell:
         return len(list)
     def automatic_0_check(self):
         list = self.list_of_surroundings()
-
-
-
-# btn = Button(
-#     center_frame, # tutaj decyzja w jakim oknie bedzie przycisk
-#     bg = "blue", # kolor
-#     text = "First button" # tekst
-# )
--- scr/main.py
+++ scr/main.py
@@ -35,56 +35,35 @@ left_frame = Frame(
 )
 left_frame.place(x=0, y=utilities.height_percentage(20))
 
+middle_frame = Frame(
+    root,
+    bg='purple',
+    width=utilities.width_percentage(80),
+    height=utilities.height_percentage(20)
+)
+middle_frame.place(x=utilities.width_percentage(20), y=utilities.height_percentage(20))
+
 player1 = Frame(
     root,
     bg='green',
     width=utilities.width_percentage(40),
-    height=utilities.height_percentage(80)
+    height=utilities.height_percentage(60)
 )
-player1.place(x=utilities.width_percentage(30), y=utilities.height_percentage(20))
+player1.place(x=utilities.width_percentage(30), y=utilities.height_percentage(40))
 
 player2 = Frame(
     root,
     bg='blue',
     width=utilities.width_percentage(30),
-    height=utilities.height_percentage(80)
+    height=utilities.height_percentage(60)
 )
-player2.place(x=utilities.width_percentage(70), y=utilities.height_percentage(20))
+player2.place(x=utilities.width_percentage(70), y=utilities.height_percentage(40))
 
+Cell.switch_to_player2 = Cell.bulid_player_grid("player2",player2)
 
-def bulid_play1_grid():
-    for x in range(settings.button_rows):
-        for y in range(settings.button_columns):
-            button = Cell(x, y)
-            button.create_btn_object(
-                player1,
-                height=settings.button_height,
-                width=settings.button_width
-            )
-            button.cell_btn_object.grid(column=x, row=y)
-            Cell.player1_cells.append(button)
+Cell.creating_labels(left_frame,left_frame,middle_frame)
 
-def bulid_play2_grid():
-    for x in range(settings.button_columns):
-        for y in range(settings.button_rows):
-            button = Cell(x, y)
-            button.create_btn_object(
-                player2,
-                height=settings.button_height,
-                width=settings.button_width
-            )
-            button.cell_btn_object.grid(column=x, row=y)
-            Cell.player2_cells.append(button)
-
-Cell.switch_to_player2 = bulid_play2_grid
-
-Cell.create_player1_ship_count(left_frame)
-Cell.player1_ships_count_label_object.place(x=0, y=0)
-Cell.create_player2_ship_count(left_frame)
-Cell.player2_ships_count_label_object.place(x=0, y=50)
-
-bulid_play1_grid()
-
+Cell.bulid_player_grid("player1",player1)
 
 # uruchomienie okna
 root.mainloop()
